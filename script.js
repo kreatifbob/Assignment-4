9c5d97a700ba460fb38162d1ac8d94ce73ce4871
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-13 20:09:50 +1000

    Update script.js
    Update comments

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// variables elements html to js's
+
 let begin = document.querySelector("#landingpage")
 
+// variables elements html to js's
 
+// timer elements
+// firstpage elements 
+// questions page elements 
+// questions 
+// bonustime time
 
 // add listener when clicking strat start the page //functıon for clıck
 // create an array of q's and answers
