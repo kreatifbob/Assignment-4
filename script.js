a44405f212e7bb531c25f207088cb06db337a45d
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-13 20:04:42 +1000

    Create script.js
    Created .js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// variables elements html to js's
+let begin = document.querySelector("#landingpage")
+
+
+
+// add listener when clicking strat start the page //functıon for clıck
+// create an array of q's and answers
