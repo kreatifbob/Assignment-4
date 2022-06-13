b2ce0a3bf9ee367c204865d563379c8aa29d0182
Tanel Colak <88058585+kreatifbob@users.noreply.github.com>
2022-06-13 20:28:39 +1000

    Update script.js
    Added more comments and function for page mode

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ let begin = document.querySelector("#landingpage")
 // questions 
 // bonustime time
 
+// timer function 
+// page mode function ın order hide question page
+
+function pageMode(){
+    if(empty === true){                  //replace empty with startpage
+        //questionspage display none
+        //landingpage display default
+    }else if(empty === false){          //replace empty with questionspage
+        //questionspage display default
+        //landingpage display none
+    }
+}
+
 // add listener when clicking strat start the page //functıon for clıck
 // create an array of q's and answers
